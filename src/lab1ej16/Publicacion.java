f7071914740abfea55cec145bb9ef5479b76b46a
USUARIO <USUARIO@DESKTOP-TSBE0BF>
2016-08-15 17:19:16 -0500

    Version 1.0

---
+++ src/lab1ej16/Publicacion.java
@@ -0,0 +1,30 @@
+
+package lab1ej16;
+import java.util.Scanner;
+public class Publicacion {
+    
+    private String titulo, precio;
+
+    Scanner lector=new Scanner(System.in);
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+    
+    public void Cargar_Datos(){
+        System.out.print("Ingrese el titulo del libro: ");
+        titulo=lector.next();
+        System.out.print("Ingrese el precio del libro: ");
+        precio=lector.next();
+    }
+    
+    public void Mostrar(){
+        System.out.println("El titulo del libro es: "+titulo);
+        System.out.println("El precio del libro es: "+precio);
+    }
+    
+}
