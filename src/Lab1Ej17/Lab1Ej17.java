b019f6a64e1c051eeb848ff483830ff9b11e3b7b
USUARIO <USUARIO@DESKTOP-TSBE0BF>
2016-08-15 17:57:29 -0500

    Version 2.0

--- src/Lab1Ej17/Lab1Ej17.java
+++ src/Lab1Ej17/Lab1Ej17.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Se dispone de la clase Publicación del ejercicio anterior y se desea crear una 
+nueva clase base llamada Ventas que obtenga un arreglo con las ventas del último 
+semestre de una determinada publicación. Esta clase debe tener funciones miembros 
+Leer() y Mostrar() que obtenga y visualice las citadas ventas. Modificar las clases 
+Libro y Disco, lea y visualice las publicaciones.
  */
 package Lab1Ej17;
 
